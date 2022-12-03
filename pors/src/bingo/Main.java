f7b9490a879d6564ce5fc69f1c115529aa5730cb
STIXGT <stiveguano@gmail.com>
2022-12-03 00:27:23 -0500

    pors

---
+++ pors/src/bingo/Main.java
@@ -0,0 +1,13 @@
+package bingo;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Bingo bingo90 = new Bingo90();
+		bingo90.jugar();
+
+	}
+
+}
